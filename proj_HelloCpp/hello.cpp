73a135539cfc990c6681740037dee715df340f6a
aidenwhite27 <aidenwhite27@gmail.com>
2021-09-16 11:15:01 -0700

    Proj_HelloCpp first hello program

---
+++ proj_HelloCpp/hello.cpp
@@ -0,0 +1,11 @@
+// @file hello.cpp
+// @brief This program prints "Hello C++!" to the console
+// @author Aiden White @aiwhite@chapman.edu
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello C++!" << std::endl;
+    return 0;
+}
