ce5857cea4ff782fc2d182810ff8d4d45e9268da
aidenwhite27 <aidenwhite27@gmail.com>
2021-09-16 11:24:40 -0700

    Proj_HelloCPP hello with namespace

---
+++ proj_HelloCpp/hello_namespace.cpp
@@ -0,0 +1,11 @@
+// @file hello_namespace.cpp
+// @brief Outputs Hello C++! and uses a namespace
+// @author Aiden White aiwhite@chapman.edu
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Hello C++!" << endl;
+	return 0;
+}
